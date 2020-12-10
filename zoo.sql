66f95917a1d451bee4d9a74f0755210b52fc22f8
cgcarlosg <cgcarlosg@hotmail.com>
2020-12-09 19:27:18 -0500

    add information to teh file according to the zoo tutorial

--- zoo.sql
+++ zoo.sql
@@ -128,7 +128,7 @@ SELECT name FROM world WHERE
     name LIKE '%u%' and name LIKE '%a%' and name LIKE '%o%'
     and name LIKE '%i%' and name LIKE '%e%' and name NOT LIKE '% %' 
 
--- nobel Nobel Laureates
+-- Nobel Laureates
 -- Winners from 1950
 
 -- 1. Change the query shown so that it displays Nobel prizes for 1950.
@@ -149,4 +149,151 @@ SELECT winner
 
 SELECT yr, subject FROM nobel WHERE
     winner = 'Albert Einstein'
+
+-- Recent Peace Prizes
+
+-- 4. Give the name of the 'Peace' winners since the year 2000, including 2000.
+
+SELECT winner FROM nobel
+WHERE subject = 'peace' AND yr >= 2000
+
+-- Literature in the 1980's
+
+-- 5. Show all details (yr, subject, winner) of the Literature prize winners for 1980 to 1989 inclusive.
+
+SELECT yr, subject, winner FROM nobel
+WHERE subject = 'literature' AND yr >= 1980 AND yr <= 1989
+
+-- Only Presidents
+
+-- 6. Show all details of the presidential winners:
+-- Theodore Roosevelt, Woodrow Wilson, Jimmy Carter, Barack Obama
+
+SELECT yr, subject, winner FROM nobel
+WHERE winner = 'Theodore Roosevelt' OR winner = 'Woodrow Wilson'
+OR winner = 'Jimmy Carter' OR winner = 'Barack Obama'
+
+-- John
+
+-- 7. Show the winners with first name John
+
+SELECT winner FROM nobel WHERE left(winner, 4) = 'john'
+
+-- Chemistry and Physics from different years
+
+-- 8. Show the year, subject, and name of Physics winners for 1980 together with the Chemistry winners for 1984.
+
+SELECT yr, subject, winner FROM nobel WHERE  (subject = 'Physics' AND yr = 1980) OR (subject = 'Chemistry' AND yr = 1984)
+
+-- Exclude Chemists and Medics
+
+-- 9. Show the year, subject, and name of winners for 1980 excluding Chemistry and Medicine
+
+SELECT yr, subject, winner FROM nobel WHERE
+yr = 1980 AND NOT (subject = 'Chemistry' OR subject = 'Medicine')
+
+-- Early Medicine, Late Literature
+
+-- 10. Show year, subject, and name of people who won a 'Medicine' prize in an early year (before 1910, not including 1910) 
+-- together with winners of a 'Literature' prize in a later year (after 2004, including 2004)
+
+SELECT yr, subject, winner FROM nobel WHERE
+(subject = 'Medicine' AND yr < 1910) OR (subject = 'Literature' AND yr >= 2004)
+
+-- Harder Questions
+--Umlaut
+
+-- 11. Find all details of the prize won by PETER GRÜNBERG
+
+SELECT * FROM nobel WHERE winner = 'PETER GRÜNBERG'
+
+-- Apostrophe
+
+-- 12. Find all details of the prize won by EUGENE O'NEILL
+-- Escaping single quotes
+
+SELECT * FROM nobel WHERE winner = 'EUGENE O''NEILL'
+
+-- Knights of the realm
+
+-- 13. Knights in order
+-- List the winners, year and subject where the winner starts with Sir. Show the the most recent first, then by name order.
+
+SELECT winner, yr, subject FROM nobel WHERE winner LIKE 'Sir%' ORDER BY yr DESC
+
+-- Chemistry and Physics last
+
+-- 14. The expression subject IN ('Chemistry','Physics') can be used as a value - it will be 0 or 1.
+-- Show the 1984 winners and subject ordered by subject and winner name; but list Chemistry and Physics last.
+
+SELECT winner, subject FROM nobel WHERE yr = 1984 ORDER BY 
+subject IN ('Physics', 'Chemistry'), subject, winner 
+
+--SELECT within SELECT Tutorial
+-- Using nested SELECT
+
+--Bigger than Russia
+
+-- 1. List each country name where the population is larger than that of 'Russia'. 
+
+SELECT name FROM world WHERE population > (SELECT population FROM
+world WHERE name = 'Russia')
+
+-- Richer than UK
+
+-- 2. Show the countries in Europe with a per capita GDP greater than 'United Kingdom'. Per Capita GDP
+
+SELECT name FROM world WHERE continent = 'europe' And gdp/population > (SELECT GDP/population FROM world where name = 'United kingdom')
+
+-- Neighbours of Argentina and Australia
+
+-- 3. List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country.
+
+SELECT name, continent FROM world WHERE continent IN (SELECT continent FROM world WHERE name IN ('Argentina', 'Australia')) ORDER BY name
+
+-- Between Canada and Poland
+
+-- 4. Which country has a population that is more than Canada but less than Poland? Show the name and the population.
+
+SELECT name, population FROM world WHERE population > (SELECT population FROM world WHERE name = 'canada') AND 
+population < (SELECT population FROM world WHERE name = 'poland')
+
+-- Percentages of Germany
+
+-- 5. Germany (population 80 million) has the largest population of the countries in Europe. Austria (population 8.5 million) has 11% of the population of Germany
+-- Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany.
+-- The format should be Name, Percentage for example:
+
+SELECT name, CONCAT (Round(100 * population /(SELECT population
+FROM world WHERE name = 'germany')), '%') FROM world WHERE
+continent = 'europe'
+
+-- Bigger than every country in Europe
+
+-- 6. Which countries have a GDP greater than every country in Europe? [Give the name only.] (Some countries may have NULL gdp values)
+
+SELECT name FROM world WHERE GDP > All(SELECT GDP FROM world WHERE
+continent = 'europe' and GDP > 0)
+
+-- Largest in each continent
+
+-- 7. Find the largest country (by area) in each continent, show the continent, the name and the area:
+
+SELECT continent, name, area FROM world B WHERE area >= ALL (SELECT area FROM world A WHERE A.continent = B.continent AND area > 0)
+
+-- First country of each continent (alphabetically)
+
+-- 8. List each continent and the name of the country that comes first alphabetically. 
+
+SELECT continent, name FROM world A WHERE name <= ALL(SELECT name FROM world B WHERE A.continent = B.continent)
+
+-- Difficult Questions That Utilize Techniques Not Covered In Prior Sections
+
+-- 9. Find the continents where all countries have a population <= 25000000. Then find the names of the countries associated with these continents. Show name, continent and population.
+
+SELECT name, continent, population FROM world WHERE continent IN (SELECT continent FROM world A WHERE 25000000 >= (SELECT MAX(population) FROM world B WHERE A.continent = B.continent))
+
+-- 10. Some countries have populations more than three times that of any of their neighbours (in the same continent). Give the countries and continents.
+
+SELECT name,continent FROM world A WHERE population > ALL(SELECT 3 * population FROM world B WHERE A.continent = B.continent AND A.name <> B.name)
 
